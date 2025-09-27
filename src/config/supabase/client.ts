d42d00c2337309d0809bfa98a459831d1f7605a2
tse-coder <shewamaretsegaye@gmail.com>
2025-09-27 05:19:55 +0300

    try to fix undefined supabase key error on vercel

--- src/config/supabase/client.ts
+++ src/config/supabase/client.ts
@@ -1,9 +1,10 @@
 import { createClient } from '@supabase/supabase-js';
-import dotenv from 'dotenv';
 
-dotenv.config();
+const supabaseUrl = process.env.SUPABASE_URL;
+const supabaseKey = process.env.SUPABASE_ANON_KEY;
 
-export const supabase = createClient(
-	process.env.SUPABASE_URL || '',
-	process.env.SUPABASE_ANON_KEY || ''
-);
+if (!supabaseUrl || !supabaseKey) {
+	throw new Error('Supabase URL or Anon Key is missing in environment variables');
+}
+
+export const supabase = createClient(supabaseUrl, supabaseKey);
